4d6e70ef48670b3049dbed563d99ac430658860e
Cruelasss <irula5198@gmail.com>
2025-11-27 15:16:13 +0700

    Refactor: PenyediaViewModel.kt

--- app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,32 @@
 package com.example.myroomsatu.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroomsatu.repositori.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        // Initializer untuk HomeViewModel
+        initializer {
+            _root_ide_package_.com.example.myroomsatu.viewmodel.HomeViewModel(
+                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
+        // Initializer untuk EntryViewModel
+        initializer {
+            EntryViewModel(
+                repositoriSiswa = aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+    }
+}
+
+/**
+
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
